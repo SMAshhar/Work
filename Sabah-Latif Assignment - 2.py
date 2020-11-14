28ba27618fbf58ef3105d67bad6cc76cef904fe4
SMAshhar <63975782+SMAshhar@users.noreply.github.com>
2020-11-15 02:45:28 +0500

    Add files via upload

--- Sabah-Latif Assignment - 2.py
+++ Sabah-Latif Assignment - 2.py
@@ -8,16 +8,16 @@
 ###     ii,     Load data from the data file                                            ###
 ###     iii,    check if details match, if match, go ahead, if not, return to step i    ###
 ###     iv,     if go ahead, message to the user for successful login and print card.   ###
-###     v,      exit                                                                    ###
+###     v,      Recall 1.                                                               ###
 ### 3. If registeration is choosen, it will go down the following path:                 ###
 ###     i,      Input details (CNIC)                                                    ###
 ###     ii,     If CNIC not registered, go ahead, else return to step 1                 ###
 ###     iii,    Input further details (name, father's name, course etc)                 ###
 ###     iv,     Generate enrollment no.                                                 ###
 ###     v,      Store data for later use.                                               ###
-###     vi,     Generaet student card                                                   ###
+###     vi,     Generate student ID card                                                ###
 ###     vii,    Display student ID card                                                 ###
-###     viii,   Exit                                                                    ###
+###     viii,   Recall 1.                                                               ###
 ###########################################################################################
 ###########################################################################################
 
@@ -25,10 +25,43 @@ import json
 import csv
 
 ###########################################################################################
+############################# Defining the welcome function ###############################
+###########################################################################################
+
+def welcome():
+    # User will choose the path to choose from the following statement. 
+    choice = input("""#################################################
+
+        Please select your preference:
+
+        Press 1 for Login
+        Press 2 for New Registeration
+        Choice : """)
+
+    if choice == "1":
+        login()
+
+    elif choice == "2":
+        register()
+
+
+###########################################################################################
 ############################### Defining the login function ###############################
 ###########################################################################################
 
 def login():
+
+    # This print section will be displaed as the successful transition to the login path
+
+    print("""
+                
+    *************************************
+                
+        Welcome to the Login section.
+                
+    *************************************
+                
+    """)
 
     # Conditioning for while loop. As long as check is 0, loop will keep running.
     check = 0
@@ -56,8 +89,9 @@ def login():
                                    
                 print(f"""       
                 \t\t========================================================\t
-                \t\tABC University for Computer Sceinces
+                \t\t        ABC University for Computer Sceinces
                 \t\t========================================================\t
+                                    \t\t STUDENT ID CARD
 
                 \t\tName = {dict_1[username][1]} {dict_1[username][2]}
                 \t\tFather's Name = {dict_1[username][3]}
@@ -70,10 +104,13 @@ def login():
                 """)
 
             # If the two conditions from line 44 aren't true, it prompt the user to try again and 
-            # will re-initiate the login sequence
+            # will re-initiate the login sequence.
 
             else:
                 print("\tData doesn't match, Please try again.")
+
+    # This will bring back the initial page for path selection.
+    welcome()
 
 ###########################################################################################
 ############################# Defining the register function ##############################
@@ -81,18 +118,18 @@ def login():
 
 def register():
 
-    # Loading data from data.json for checking against CNIC availability
+    # Loading data from data.json for checking against CNIC availability.
     with open("data.json") as j:
         dict_1 = json.load(j)
 
-    # Loading data from data.csv, which will be used to generate the new Enrollment Number
+    # Loading data from data.csv, which will be used to generate the new Enrollment Number.
     with open("data.csv", "r") as f:
         x = csv.reader(f)
         y = []
         for a in x:
             y += a
 
-    # A check for the loop which will run through the data from data.json file
+    # A check for the loop which will run through the data from data.json file.
     check = 0
  
     while check == 0:                  
@@ -100,8 +137,8 @@ def register():
         CNIC = input("\tPlease enter your CNIC : ")
 
         for a in dict_1.values():
-            # check2 will be used to communicate the findings from the for loop for registeration check for further 
-            # processing 
+            # check2 will be used to communicate the findings from the for loop for registeration 
+            # check for further processing. 
             check2 = 1
             if CNIC == a[4]:
 
@@ -110,26 +147,29 @@ def register():
 
             else:
                 continue
-        # If no match for current registered CNIC are found, check will be set to 1, thereby ending the while
-        # loop and continuing on with the next step.    
+        # If no match for current registered CNIC are found, check will be set to 1, thereby ending 
+        # the while loop and continuing on with the next step.    
         if check2 == 1:
             check = 1
 
-###########################################################################################
-############################### Starting program from here. ###############################
-###########################################################################################       
-    
+    # Will print this line after the While loop dies. It will only happen if CNIC number provided has
+    # never been used before.
     print("\tCNIC available.")
 
+    # This print section will be display the successful transition from CNIC checkup to further data
+    # collection.
     print("""
                 
-    *****
+    *************************************
                 
     Welcome to the registeration section.
                 
-    *****
+    *************************************
                 
     """)
+
+    # From here, the data collection will start. Each input will be assigned to a variable for later
+    # use in Student ID card generation.
 
     firstName = input("\tPlease Enter your First name : ")
     lastName = input("\tPlease enter your last name : ")
@@ -137,27 +177,43 @@ def register():
     course = input("\tPlease enter the applied course : ")
     password = input("\tPlease enter password for your profile : ")
 
+    #########################################################################################################
+    # Enrollment Number is generated in the following single statement. The statement is divided into two   #
+    # parts. The course[:2] will take the first two charactors from the course input. i.e. if course is     #
+    # CS50 the first part will be CS. In the same manner, if the course selection is EN30, the first part   #
+    # will be EN. The next part (which is numeric part) is int(y[-1][-4:]) + 1. y[-1] represents the last   #
+    # item from the data collected from the CSV file, and [-4:] represents the last 4 numbers from that     #
+    # number. 1 is added in them to generate a new enrollment number.                                       #
+    # Lastly, this number will be converted into string and added into the characters from the first        #
+    # part. and the sum will generate a full enrollment number, for example: "CS" + "1234" = "CS1234"       #
+    #########################################################################################################
+
     enroll = course[:2] + str(int(y[-1][-4:]) + 1)
                 
-
+    # dict_1 was taken from data.json. The dict_1 is updated with the new enrollment number. All data is 
+    # stored as a list against the key, that is the new enrollment number. CNIC is taken from the approved
+    # CNIC in line 101
     dict_1[enroll] = [password, firstName, lastName, fatherName, CNIC, course]
-    print(dict_1)
-    y = [enroll]
-                
-    print(enroll)
 
+    # To add the enrollment number on the CSV file, we need to convert the new generated enrollment number 
+    # into list. Which is done below.
+    y = [enroll]
+ 
+    # The updated dict_1 is written on the json file.
     with open("data.json", "w") as j:
         json.dump(dict_1, j)
 
+    # The CSV file is updated with the new enrollment number.
     with open("data.csv", "a", newline = "") as f:
         z = csv.writer(f, delimiter = ",")
         z.writerow(y)
 
-            
+    # After all the data is stored, the Student ID card is generated in the following format.     
     print(f"""    
                 \t\t========================================================\t
-                \t\tABC University for Computer Sceinces
+                \t\t        ABC University for Computer Sceinces
                 \t\t========================================================\t
+                                    \t\t STUDENT ID CARD
 
                 \t\tName = {firstName} {lastName}
                 \t\tFather's Name = {fatherName}
@@ -168,31 +224,25 @@ def register():
                 \t\tPlease don't loose your assigned Enrollment number
                 \t\t========================================================\t
                 """)
+    
+    # This will bring back the initial page for path selection.
+    welcome()
 
 
+###########################################################################################
+############################### Starting program from here. ###############################
+###########################################################################################  
 
-
+# The program will start with the following set of strings. 
 print("""
 #################################################
-***
+*************************************************
 
-Welcome to ABC School online portal
+      Welcome to ABC School online portal
 
-***
+*************************************************
 #################################################
 """)
 
-choice = input("""#################################################
-
-Please select your preference:
-
-    Press 1 for Login
-    Press 2 for New Registeration
-    Choice : """)
-
-if choice == "1":
-    login()
-
-elif choice == "2":
-    register()
+welcome()
 
